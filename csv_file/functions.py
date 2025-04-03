67c65b9242783413ec7934656f813c1322389b03
Raphael <raphaelhsmissias@gmail.com>
2025-04-03 11:46:26 -0300

    File names update

--- csv_file/functions.py
+++ csv_file/functions.py
@@ -2,33 +2,7 @@ import csv
 import glob
 import os
 
-def read_csv_contracts():
-    csv_archives = glob.glob('**/*.csv', recursive=True)
-
-    if not csv_archives:
-        print("Nenhum arquivo csv encontrado")
-        return
-
-    for archive in csv_archives:
-        try:
-            with open(archive, "r", encoding="ISO-8859-1") as f:
-                reader = csv.DictReader(f, delimiter=";")
-                # print("Cabeçalhos", reader.fieldnames)
-
-                contracts_arr = []
-                for line in reader:
-                    contract_number = line['Contrato']
-                    contracts_arr.append(contract_number)
-
-                # for item in formated_array:
-                #     print(item)
-                
-                # Retorna os números do contrato da planilha
-                return contracts_arr
-
-        except Exception as e:
-            print(f"Erro ao abrir o arquivo {archive}: {e}")
-
+# Faz leitura do arquivo csv e retorna todos os números de contrato
 def contract_numbers():
     csv_archives = glob.glob('**/*.csv', recursive=True)
 
@@ -43,15 +17,13 @@ def contract_numbers():
 
                 # print("Cabeçalhos", reader.fieldnames)
 
-                formated_array = []
+                contracts_arr = []
                 for line in reader:
                     number_dict = line['Contrato']
-                    formated_array.append(number_dict)
+                    contracts_arr.append(number_dict)
 
-                print(formated_array)
-
-                # for item in formated_array:
-                #     print(item)
+                # Retorna os números do contrato da planilha
+                return contracts_arr
 
         except Exception as e:
             print(f"Erro ao abrir o arquivo {archive}: {e}")
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from selenium_config.open_browser import initialize_browser
 from wfm.contracts import enter_contract, obtain_contract_info, search_contract
-from csv_config.csv_functions import read_csv_contracts, contract_numbers, save_csv, update_csv, dados
+from csv_file.functions import contract_numbers, save_csv, update_csv, dados
 
 if __name__ == "__main__":
     browser = initialize_browser()
     try:
-        contract_numbers = read_csv_contracts()
-        for number in contract_numbers:
+        contracts = contract_numbers()
+        for number in contracts:
             search_contract(browser, number)
             enter_contract(browser, number)
             contract_info = obtain_contract_info(browser)
